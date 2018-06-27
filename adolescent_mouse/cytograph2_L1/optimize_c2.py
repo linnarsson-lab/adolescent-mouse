5f7d8896a655d0701b68f7731c2973830465a245
Sten Linnarsson <sten.linnarsson@ki.se>
2018-06-27 10:59:03 +0200

    x

--- adolescent_mouse/cytograph2_L1/optimize_c2.py
+++ adolescent_mouse/cytograph2_L1/optimize_c2.py
@@ -16,15 +16,16 @@ class OptimizeC2(luigi.WrapperTask):
 	tissue = luigi.Parameter()
 
 	def requires(self) -> Iterator[luigi.Task]:
-		for a in [0.3, 1]:
-			for b in [0.3, 1, 10]:
-				for c in [0.3, 1, 10]:
-					for d in [0.3, 1, 10]:
-						for accel in [False]:
-							for normalize in [True]:
-								for k_smoothing in [5]:
-									for k in [10]:
-										for n_factors in [100]:
-											yield am.ExportL1C2(tissue=self.tissue, a=a, b=b, c=c, d=d, 
-												n_factors=n_factors, k_smoothing=k_smoothing, k=k, 
-												log=True, normalize=normalize, accel=accel)
+		# for a in [0.3, 1]:
+		# 	for b in [0.3, 1]:
+		# 		for c in [0.3, 1]:
+		# 			for d in [0.3, 1]:
+		for (a, b, c, d) in [(0.3, 1, 0.3, 1), (0.3, 0.3, 0.3, 0.3), (1, 10, 1, 10)]:
+			for accel in [False]:
+				for normalize in [True]:
+					for k_smoothing in [25, 50, 100, 250]:
+						for k in [10]:
+							for n_factors in [100]:
+								yield am.ExportL1C2(tissue=self.tissue, a=a, b=b, c=c, d=d, 
+									n_factors=n_factors, k_smoothing=k_smoothing, k=k, 
+									log=True, normalize=normalize, accel=accel)
